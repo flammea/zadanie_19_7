c296a26183b272dfb7300a056ae22d90967fbeea
Beata Markunas <beata.markunas@gmail.com>
2018-05-18 21:39:40 +0200

    Add Provider

--- src/index.js
+++ src/index.js
@@ -4,5 +4,11 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+import { Provider } from 'react-redux';
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
